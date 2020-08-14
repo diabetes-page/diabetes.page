354d00c7f14e7d864987593c9128a8d8ad5496ad
Vincent Rolfs <vincent.rolfs@gmail.com>
2020-08-14 16:06:04 +0800

    class-validator useContainer in setupValidationPipe

--- backend/src/bootstrap/bootstrap.ts
+++ backend/src/bootstrap/bootstrap.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/AppModule';
 import { setupPipes } from './pipes/setupPipes';
 import { setupInterceptors } from './interceptors/setupInterceptors';
-import { useContainer } from 'class-validator';
 import { INestApplication } from '@nestjs/common';
 
 export async function bootstrap(): Promise<INestApplication> {
@@ -10,10 +9,6 @@ export async function bootstrap(): Promise<INestApplication> {
 
   setupPipes(app);
   setupInterceptors(app);
-
-  // This is needed in order to do dependency injection for custom validators, see
-  // https://github.com/nestjs/nest/issues/528
-  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   if (process.env.NODE_ENV !== 'testing') {
     await app.listen(3000);
--- backend/src/bootstrap/pipes/setupPipes.ts
+++ backend/src/bootstrap/pipes/setupPipes.ts
@@ -1,9 +1,15 @@
-import {INestApplication, ValidationPipe} from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
+import { AppModule } from '../modules/app/AppModule';
 
 export const setupPipes = (app: INestApplication): void => {
-    app.useGlobalPipes(createValidationPipe());
+  setupValidationPipe(app);
 };
 
-const createValidationPipe = () => {
-    return new ValidationPipe();
-}
+const setupValidationPipe = (app: INestApplication) => {
+  app.useGlobalPipes(new ValidationPipe());
+
+  // This is needed in order to do dependency injection for custom validators
+  // see https://github.com/nestjs/nest/issues/528
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+};
