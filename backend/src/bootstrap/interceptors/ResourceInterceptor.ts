d21b907ca80a398c662667d5e42709e9200ad2a4
Vincent Rolfs <vincent.rolfs@gmail.com>
2020-10-03 14:39:45 +0200

    Remove unnecessary generic, unnecessary variable

--- backend/src/bootstrap/interceptors/ResourceInterceptor.ts
+++ backend/src/bootstrap/interceptors/ResourceInterceptor.ts
@@ -13,7 +13,7 @@ import { ResourceController } from '../blueprints/ResourceController';
  * This interceptor is used in order to extract only the fields that are to be exposed by a given resource.
  * It uses class-transformer in order to extract all values of the resource which are annotated with @Expose().
  * This is powerful because it allows us to specify default values to be exposed by entities, while enabling us to
- * extent these.
+ * extend these.
  *
  * In order for this transformation to work, the input object must actually be an instance of the resource class, and
  * not just some object. Because of this, the object is first transformed to an instance of the resource class using
@@ -38,17 +38,15 @@ export class ResourceInterceptor implements NestInterceptor {
 
     return next
       .handle()
-      .pipe(map(value => this.mapResource(value, controllerClass)));
+      .pipe(map((value) => this.mapResource(value, controllerClass)));
   }
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/ban-types
-  mapResource<T>(value: any, controllerClass: any): Object {
+  mapResource(value: any, controllerClass: any): Object {
     const asClass = plainToClass(controllerClass.Resource, value);
 
-    const asPlain = classToPlain(asClass, {
+    return classToPlain(asClass, {
       strategy: 'excludeAll',
     });
-
-    return asPlain;
   }
 }
