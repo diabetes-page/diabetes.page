9c3a26e68f8999d9a7c4e85ae905ebda1e5c110d
Vincent Rolfs <vincent.rolfs@gmail.com>
2020-11-25 00:08:57 +0100

    Reorganize bootstrap modules

--- backend/src/bootstrap/modules/app/AppModule.ts
+++ backend/src/bootstrap/modules/app/AppModule.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ValidationModule } from '../validation/ValidationModule';
 import { UsersModule } from '../../../domains/users/UsersModule';
 import { AuthModule } from '../../../domains/auth/AuthModule';
-import { TypeOrmModule } from './TypeOrmModule';
-import { ConfigModule } from './ConfigModule';
+import { TypeOrmModule } from '../typeOrm/TypeOrmModule';
+import { ConfigModule } from '../config/ConfigModule';
 import { AppointmentsModule } from '../../../domains/appointments/AppointmentsModule';
 
 @Module({
