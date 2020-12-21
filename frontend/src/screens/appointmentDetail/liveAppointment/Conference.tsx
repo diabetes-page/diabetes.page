2a89ff0ede187e1df38a1809d45f59832cd857dd
Vincent Rolfs <vincent.rolfs@gmail.com>
2020-12-21 18:14:18 +0100

    Use Jitsi as import

--- frontend/src/screens/appointmentDetail/liveAppointment/Conference.tsx
+++ frontend/src/screens/appointmentDetail/liveAppointment/Conference.tsx
@@ -8,6 +8,7 @@ import React, {
 import { Jitsi } from './Jitsi';
 import { Converse } from './Converse';
 import { ConferenceContext } from './ConferenceContext/ConferenceContext';
+import JitsiApi from './Jitsi/JitsiApi';
 
 export function Conference(): JSX.Element {
   const jitsiRef = useRef<HTMLDivElement>(null);
@@ -26,43 +27,32 @@ const useJitsi = (jitsiRef: RefObject<HTMLDivElement>): boolean => {
   const [userId, setUserId] = useState<string | undefined>(undefined);
 
   useEffect(() => {
-    const script = document.createElement('script');
-
-    script.src = 'https://localhost:8443/external_api.js';
-    script.async = true;
-    script.onload = (): void => {
-      const domain = 'localhost:8443';
-      const options = {
-        roomName: conferenceData!.state.room!,
-        jwt: conferenceData!.state.conferenceToken!,
-        parentNode: jitsiRef.current,
-        width: 700, // todo: figure out width/height
-        height: 700,
-        interfaceConfigOverwrite: {
-          TOOLBAR_BUTTONS: ['microphone', 'camera', 'chat', 'stats'],
-          TOOLBAR_ALWAYS_VISIBLE: true,
-          DISABLE_VIDEO_BACKGROUND: true,
-        },
-      };
-
-      // NOTE: It's a good practice to remove the conference before the page is unloaded.
-      // @ts-ignore : JitsiMeetExternalAPI is loaded into window by script
-      const jitsi = new window.JitsiMeetExternalAPI(domain, options);
-
-      jitsi.on('videoConferenceJoined', (data: Record<string, string>) => {
-        console.warn('Data got', data);
-        setUserId(data.id);
-      });
-
-      jitsi.on('incomingMessage', (data: Record<string, string>) => {
-        console.warn('incomingMessage got', data);
-      });
+    const domain = 'localhost:8443';
+    const options = {
+      roomName: conferenceData!.state.room!,
+      jwt: conferenceData!.state.conferenceToken!,
+      parentNode: jitsiRef.current,
+      width: 700, // todo: figure out width/height
+      height: 700,
+      interfaceConfigOverwrite: {
+        TOOLBAR_BUTTONS: ['microphone', 'camera', 'chat', 'stats'],
+        TOOLBAR_ALWAYS_VISIBLE: true,
+        DISABLE_VIDEO_BACKGROUND: true,
+      },
     };
 
-    document.body.appendChild(script);
+    // Todo: It's a good practice to remove the conference before the page is unloaded.
+    const jitsi = new JitsiApi(domain, options);
 
-    return () => void document.body.removeChild(script);
-  }, []);
+    jitsi.on('videoConferenceJoined', (data: Record<string, string>) => {
+      console.warn('Data got', data);
+      setUserId(data.id);
+    });
+
+    jitsi.on('incomingMessage', (data: Record<string, string>) => {
+      console.warn('incomingMessage got', data);
+    });
+  });
 
   return userId !== undefined;
 };
---
+++ frontend/src/screens/appointmentDetail/liveAppointment/Jitsi/JitsiApi.d.ts
@@ -0,0 +1,11 @@
+/**
+ * See https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-class-d-ts.html
+ * https://stackoverflow.com/questions/63189533/how-can-i-solve-error-this-expression-is-not-constructable
+ */
+
+export = JitsiApi;
+
+declare class JitsiApi {
+  constructor(domain: string, options: Record<string, any>);
+  on(event: string, callback: (data: Record<string, string>) => void): void;
+}
