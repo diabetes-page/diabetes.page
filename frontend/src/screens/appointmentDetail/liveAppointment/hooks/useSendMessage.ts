3ebbd2cda1b2838125df88525cde17bdc0d47a8c
Vincent Rolfs <vincent.rolfs@gmail.com>
2020-12-22 11:58:06 +0100

    Only try to send message if possible

--- frontend/src/screens/appointmentDetail/liveAppointment/hooks/useSendMessage.ts
+++ frontend/src/screens/appointmentDetail/liveAppointment/hooks/useSendMessage.ts
@@ -3,9 +3,15 @@ import { ConferenceContext } from '../ConferenceContext/ConferenceContext';
 
 export const useSendMessage = (): ((message: string) => void) => {
   const conference = useContext(ConferenceContext);
-  const chat = conference!.state.converseAPI!.chatviews.get(
-    conference!.state.room! + '@muc.meet.jitsi', // Todo: refactor magic string
-  );
 
-  return (message: string) => void chat.sendMessage(message);
+  return (message: string): void => {
+    if (!conference || !conference.state.converseAPI || !conference.state.room) {
+      return;
+    }
+
+    const chat = conference.state.converseAPI.chatviews.get(
+        conference.state.room + '@muc.meet.jitsi', // Todo: refactor magic string
+    );
+    chat.sendMessage(message);
+  };
 };
