2f0a6f68a0cc4fa2aba8466b2d06241c2578d150
Vincent Rolfs <vincent.rolfs@gmail.com>
2021-01-04 15:39:03 +0100

    Send messages to chat as user

--- frontend/src/screens/appointmentDetail/liveAppointment/chat/Chat.tsx
+++ frontend/src/screens/appointmentDetail/liveAppointment/chat/Chat.tsx
@@ -1,9 +1,11 @@
 import React, { useCallback, useState } from 'react';
 import 'strophejs-plugin-muc';
 import { useProcessMessages } from './useProcessMessages';
-import { Text } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
+import { useSendMessage } from '../utilities/hooks/useSendMessage';
 
 export function Chat(): JSX.Element {
+  const [onInputChange, onSendMessage] = useOnSendMessage();
   const [messages, setMessages] = useState<string[]>([]);
   const displayMessage = useCallback(
     (msg: string) => setMessages([...messages, msg]),
@@ -12,10 +14,30 @@ export function Chat(): JSX.Element {
   useProcessMessages(displayMessage);
 
   return (
-    <>
+    <View>
+      <Text style={{ fontWeight: 'bold' }}>Chat</Text>
       {messages.map((message, index) => (
         <Text key={index}>{message}</Text>
       ))}
-    </>
+      <View style={{ display: 'flex', flexDirection: 'row' }}>
+        <TextInput
+          style={{ backgroundColor: 'white' }}
+          onChangeText={onInputChange}
+        />
+        <Button title="Absenden" onPress={onSendMessage} />
+      </View>
+    </View>
   );
 }
+
+const useOnSendMessage = (): [(text: string) => void, () => void] => {
+  const [input, setInput] = useState('');
+  const sendMessage = useSendMessage();
+  const onInputChange = useCallback((text) => setInput(text), [setInput]);
+  const onSendMessage = useCallback(() => sendMessage(input), [
+    sendMessage,
+    input,
+  ]);
+
+  return [onInputChange, onSendMessage];
+};
