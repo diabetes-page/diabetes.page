2cdb93a75997b1717ae38272b810e19fbbcd3261
Vincent Rolfs <vincent.rolfs@gmail.com>
2021-01-19 20:04:27 +0100

    Optimize imports

--- backend/src/domains/users/services/UsersService.ts
+++ backend/src/domains/users/services/UsersService.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/User.entity';
-import { FindManyOptions, FindOneOptions } from 'typeorm';
 import { hash } from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
 
