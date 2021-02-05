7a9ee5318dc3dbee0d46d890e8f0617cd3210bc2
Vincent Rolfs <vincent.rolfs@gmail.com>
2021-02-05 15:04:51 +0100

    Add type to RequestUser decorator

--- backend/src/blueprints/decorators/RequestUser.ts
+++ backend/src/blueprints/decorators/RequestUser.ts
@@ -1,7 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '../../domains/users/entities/User.entity';
 
 export const RequestUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  (data: unknown, ctx: ExecutionContext): User => {
     const request = ctx.switchToHttp().getRequest();
     return request.user;
   },
