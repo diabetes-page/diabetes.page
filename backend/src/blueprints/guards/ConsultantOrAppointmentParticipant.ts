5b74038e9fc56a34376383b1a14830ad7b2af766
Vincent Rolfs <vincent.rolfs@gmail.com>
2021-03-05 18:57:30 +0100

    Use efficient SQL query in ConsultantOrAppointmentParticipant

--- backend/src/blueprints/guards/ConsultantOrAppointmentParticipant.ts
+++ backend/src/blueprints/guards/ConsultantOrAppointmentParticipant.ts
@@ -1,6 +1,4 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { findIndex } from 'lodash';
-import { Appointment } from '../../domains/appointments/entities/Appointment.entity';
 import { User } from '../../domains/users/entities/User.entity';
 import { WorkingGroup } from '../../domains/workingGroups/entities/WorkingGroup.entity';
 
@@ -14,50 +12,26 @@ export class ConsultantOrAppointmentParticipant implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const user: User = request.user;
+    const isConsultant = !!(await user.loadAsConsultant());
 
-    const workingGroupId: string | undefined =
-      request.params[this.workingGroupIdParam];
-    const appointmentId: string | undefined =
-      request.params[this.appointmentIdParam];
+    const query = WorkingGroup.createQueryBuilder('workingGroup')
+      .select('workingGroup.id')
+      .innerJoin('workingGroup.appointments', 'appointment')
+      .where('workingGroup.id = :workingGroupId', {
+        workingGroupId: request.params[this.workingGroupIdParam],
+      })
+      .andWhere('appointment.id = :appointmentId', {
+        appointmentId: request.params[this.appointmentIdParam],
+      });
 
-    if (!workingGroupId || !appointmentId) {
-      return false;
+    if (!isConsultant) {
+      query
+        .innerJoin('workingGroup.users', 'user')
+        .andWhere('user.id = :userId', {
+          userId: user.id,
+        });
     }
 
-    const workingGroup = await WorkingGroup.findOne(workingGroupId, {
-      relations: ['users'],
-    });
-    const appointment = await Appointment.findOne(appointmentId, {
-      relations: ['workingGroups'],
-    });
-
-    if (!workingGroup || !appointment) {
-      return false;
-    }
-
-    const appointmentInWorkingGroup =
-      -1 !==
-      findIndex(
-        appointment.workingGroups,
-        (appointmentGroup: WorkingGroup) =>
-          appointmentGroup.id === workingGroup.id,
-      );
-
-    if (!appointmentInWorkingGroup) {
-      return false;
-    }
-
-    if (await user.loadAsConsultant()) {
-      return true;
-    }
-
-    const userInWorkingGroup =
-      -1 !==
-      findIndex(
-        workingGroup.users,
-        (groupUser: User) => groupUser.id === user.id,
-      );
-
-    return userInWorkingGroup;
+    return !!(await query.getOne());
   }
 }
