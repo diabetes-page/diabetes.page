feae77fe4f0d33de81969049911b0946cf38b52d
Vincent Rolfs <vincent.rolfs@gmail.com>
2021-03-09 19:45:56 +0100

    Use fallbackLangauge from config in I18nModule

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,3 +1,4 @@
+import { i18nConfig } from './i18n';
 import { jitsiConfig } from './jitsi';
 import { securityConfig } from './security';
 
@@ -5,4 +6,5 @@ export const config = (): Record<string, any> => ({
   environment: process.env.NODE_ENV,
   security: securityConfig(),
   jitsi: jitsiConfig(),
+  i18n: i18nConfig(),
 });
--- backend/src/config/utilities/findEnvOrFail.ts
+++ backend/src/config/utilities/findEnvOrFail.ts
@@ -2,7 +2,9 @@ export const findEnvOrFail = (envKey: string): string => {
   const value = process.env[envKey];
 
   if (typeof value === 'undefined') {
-    throw new Error(`Environment variable ${envKey} is not set!`);
+    throw new Error(
+      `Environment variable ${envKey} is not set! Please set it in your .env file.`,
+    );
   }
 
   return value;
