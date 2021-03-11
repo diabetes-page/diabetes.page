07b181c25747ae42836e0455fdc74b48527c4eb5
Vincent Rolfs <vincent.rolfs@gmail.com>
2021-03-11 17:46:47 +0100

    Start implementing initial page

---
+++ frontend/src/pageImplementations/initial/Initial.tsx
@@ -0,0 +1,29 @@
+import { includes } from 'lodash';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Loader } from '../../components/Loader';
+import { SET_LOGGED_IN } from '../../redux/login/actions';
+import { RootState } from '../../redux/root/state';
+
+export function Initial(): JSX.Element {
+  const loginLoading = useSelector((state: RootState) =>
+    includes(state.loading.initial, SET_LOGGED_IN),
+  );
+  const loggedIn = useSelector((state: RootState) => !!state.login.loggedIn);
+  let content: JSX.Element;
+
+  if (loginLoading) {
+    content = <Loader />;
+  } else if (loggedIn) {
+    content = <Loader />;
+  } else {
+    content = <Loader />;
+  }
+
+  return (
+    <>
+      {/*<Auth />*/}
+      {content}
+    </>
+  );
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,20 +1,6 @@
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
 import React from 'react';
-import Link from '../components/ui/Link';
+import { Initial } from '../pageImplementations/initial/Initial';
 
-export default function Home(): JSX.Element {
-  return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-      </Box>
-    </Container>
-  );
+export default function InitialPage(): JSX.Element {
+  return <Initial />;
 }
