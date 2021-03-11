62d07a8136841f88654afc8c154a438cbe5b9409
Tom Diacono <thomas.diacono1999@gmail.com>
2021-03-11 22:20:47 +0000

    DP-42 Removing commented code

--- backend/src/domains/trainings/routes/showTrainings/test/showTrainings.steps.ts
+++ backend/src/domains/trainings/routes/showTrainings/test/showTrainings.steps.ts
@@ -10,5 +10,4 @@ Then(/^the response contains an array of trainings$/, function () {
   expect(this.response.body.trainings[0])
     .to.have.property('name')
     .to.equal('Turing Machines by Jesse Pinkman');
-  // expect(options.particles.color).to.be.an("object").to.have.property("value").to.equal("#fff"); // this is a little more complex, but still really clear
 });
