1639b7c3076151bafccb565155d84c6a6c278a40
Joe Hewett <joehewett1@gmail.com>
2021-03-15 21:56:53 +0000

    Add calendar click, select, change handlers

--- frontend/src/app/showCalendar/Calendar.tsx
+++ frontend/src/app/showCalendar/Calendar.tsx
@@ -1,10 +1,16 @@
 // organize-imports-ignore
-import FullCalendar from '@fullcalendar/react';
+import FullCalendar, {
+  EventApi,
+  DateSelectArg,
+  EventClickArg,
+  EventContentArg,
+  formatDate,
+} from '@fullcalendar/react';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { makeStyles } from '@material-ui/core';
-import React, { SetStateAction } from 'react';
+import React, { SetStateAction, useEffect } from 'react';
 import { useSelector } from '../../redux/root/hooks';
 import { AppointmentInWorkingGroupResource } from '../../utilities/requests/requests';
 
@@ -15,6 +21,18 @@ type CalendarProps = {
   >;
 };
 
+const handleDateSelect = (selectInfo: DateSelectArg): void => {
+  console.log('Selected date: ', selectInfo);
+};
+
+const handleEventClick = (clickInfo: EventClickArg): void => {
+  console.log('Event clicked: ', clickInfo);
+};
+
+const handleEvents = (events: EventApi[]): void => {
+  console.log('Selected date: ', events);
+};
+
 // TODO: Think about duplicate code here that we could reduce
 export function Calendar({
   appointments,
@@ -23,6 +41,10 @@ export function Calendar({
   const userId = useSelector((state) => state.user.id);
   const classes = useStyles();
 
+  useEffect(() => {
+    console.log('Testing');
+    console.log(appointments);
+  }, []);
   // This component doesn't get rendered until appointments has been loaded in the parent
   return (
     <FullCalendar
@@ -30,7 +52,11 @@ export function Calendar({
       initialView="dayGridMonth"
       nowIndicator={true}
       editable={true}
+      selectable={true}
       initialEvents={[{ title: 'nice event', start: new Date() }]}
+      select={handleDateSelect}
+      eventClick={handleEventClick}
+      eventsSet={handleEvents}
     />
   );
 }
